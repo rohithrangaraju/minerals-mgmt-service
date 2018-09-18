5a4ddd5454fa51d44df23db4c5ab7d34f059ec89
Rohith Krishna <rangaraju963@gmail.com>
2018-09-19 02:51:27 +0530

    - Added migration script

---
+++ migration_script.py
@@ -0,0 +1,16 @@
+from flask_mongoalchemy import MongoAlchemy
+import flask
+# .
+# ├── app
+# │   ├── __init__.py
+# │   ├── main
+# │   │   ├── controller
+# │   │   │   └── __init__.py
+# │   │   ├── __init__.py
+# │   │   ├── model
+# │   │   │   └── __init__.py
+# │   │   └── service
+# │   │       └── __init__.py
+# │   └── test
+# │       └── __init__.py
+# └── requirements.txt
